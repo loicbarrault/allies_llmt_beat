8238eb279805a337e439919a1a4f2ed34d4c991e
Magdalena Biesialska <magdalena.biesialska@upc.edu>
2020-10-05 20:50:28 +0100

    Fixed ENVNAME in environment.yml and added OpenKiwi required packages

--- install.bash
+++ install.bash
@@ -7,38 +7,37 @@ conda update -n base conda
 conda config --set show_channel_urls True
 
 echo "This will create a python env named 'beat_allies' and install the necessary tools for LLMT"
-read -n 1 -p "Proceed? [y/n]" choice
+read -n 1 -p "Proceed ([y]/n)? " choice
 case "$choice" in 
-  y|Y ) echo "Let's go!";;
-  n|N ) echo "Quitting..."; exit 1;;
-  * ) echo "invalid input"; exit 1;;
+  y|Y ) printf "\nLet's go!";;
+  n|N ) printf "\nQuitting..."; exit 1;;
+  * ) printf "\ninvalid input"; exit 1;;
 esac
 
 
-sed -i 's/ENVNAME/$envname' environment.yml
+#sed -i 's/ENVNAME/$envname' environment.yml
 conda env create -f environment.yml
 #conda init bash
 source activate $envname
 
 echo "Installing third party tools: nmtpytorch"
-read -n 1 -p "Proceed? [y/n]: " choice
+read -n 1 -p "Proceed ([y]/n)? " choice
 case "$choice" in 
-  y|Y ) echo "Let's go!";;
-  n|N ) echo "Quitting..."; exit 1;;
-  * ) echo "invalid input"; exit 1;;
+  y|Y ) printf "\nLet's go!";;
+  n|N ) printf "\nQuitting..."; exit 1;;
+  * ) printf "\ninvalid input"; exit 1;;
 esac
 
 cd nmtpytorch
 python setup.py develop
 cd ..
 
+echo "Installing OpenKiwi"
+cd openkiwi
+python setup.py
+cd ..
+
 echo "You can run the baseline system with the following command:"
 echo "beat --prefix `pwd`/beat exp run loicbarrault/loicbarrault/translation_ll_dev/1/translation_ll_dev"
 echo "(be sure to export CUDA_VISIBLE_DEVICES=X where X is the GPU you want to use)"
-
-
-
-
-
-
 
